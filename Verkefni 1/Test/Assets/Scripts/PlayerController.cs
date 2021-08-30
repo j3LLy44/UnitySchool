3b6bd30074fc32b7e86ce324131ac1b171ac7b66
j3LLy44 <hjalmarhs44@gmail.com>
2021-08-30 14:34:44 +0000

    .

---
+++ Verkefni 1/Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Hjálmar Húnfjörð
+public class PlayerController : MonoBehaviour
+{
+
+    //Variables
+    private float speed = 10f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Get inputs from unity
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Vehicle moves
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Vehicle turns
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
