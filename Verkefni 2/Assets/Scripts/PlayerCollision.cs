2f2d1ea84cfa3d0eeb44024683fca1b4349f32df
j3LLy44 <hjalmarhs44@gmail.com>
2021-09-03 20:24:43 +0000

    .

--- Verkefni 2/Assets/Scripts/PlayerCollision.cs
+++ Verkefni 2/Assets/Scripts/PlayerCollision.cs
@@ -11,9 +11,10 @@ public class PlayerCollision : MonoBehaviour
     public Color32 obstacleColor = new Color32(255, 55, 55, 255);
 
     //Lives
-    public float playerLives = 500f; //How many lives the player has
+    public float playerLives = 500f;//How many lives the player has
     public float lifeSteal = 10f;   //how many lives are taken when the player collides with obstacle
     public float lifeDrain = 2f;    //how many lives per second the player loses while in contact with obstacle
+    public float livesAdded = 25f;     //Lives to add when the player collides with a coin
 
     //Various variables
     private bool collided_Obstacle;
@@ -26,7 +27,14 @@ public class PlayerCollision : MonoBehaviour
             collided_Obstacle = true;
 
             playerLives -= 10f;
+        }
 
+        if (other.collider.tag == "Coin")
+        {
+            Debug.Log("COIN COLLISION");
+            Destroy(other.collider.gameObject);
+
+            playerLives += livesAdded;
         }
     }
 
